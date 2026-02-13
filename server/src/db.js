a7929ea48b729601de8964c6dca08ccfe8068a88
Bryan Larez <bryan.larez@gmail.com>
2026-02-13 17:59:01 -0500

    fix: force ipv4 in db connection

--- server/src/db.js
+++ server/src/db.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    family: 4 // Force IPv4 to resolve Render ENETUNREACH errors
 });
 
 module.exports = {
